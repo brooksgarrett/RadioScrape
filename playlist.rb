0b475ac7ed76e8d1fffd2f3ad8add12f98de8def
Brooks Garrett <brooks@brooksgarrett.com>
2015-03-05 22:26:01 -0500

    Config update

--- playlist.rb
+++ playlist.rb
@@ -12,8 +12,6 @@ config = JSON.parse(redis.get('config'))
 
 RSpotify.authenticate(config['client_id'],config['client_secret'])
 
-station = "octane"
-
 # Set the options hash with our authentication information and userid
 options = {}
 credentials = {}
@@ -30,29 +28,29 @@ options['credentials'] = credentials
 user = RSpotify::User.new(options)
 
 # Do we have a playlist for the station?
-playlist = nil
 
-
-user.playlists.each do |temp_playlist|
+config['stations'].each do |station|
+  playlist = nil
+  user.playlists.each do |temp_playlist|
     if (temp_playlist.name == station)
-        playlist = temp_playlist
-        break
+      playlist = temp_playlist
+      break
     end
-end
+  end
 
-# If not then create it
-if (playlist == nil)
+  # If not then create it
+  if (playlist == nil)
     playlist = user.create_playlist!(station)
-end
+  end
 
-list = redis.zrevrange('playlist:octane', 0, 50)
-playlist_tracks = []
+  list = redis.zrevrange('playlist:' + station, 0, 50)
+  playlist_tracks = []
 
-list.each do |song|
-  data = JSON.parse(redis.get(song))
-  data['artist'] = data['artist'].gsub(':', ' ')
-  data['song'] = data['song'].gsub(':', ' ')
-  begin
+  list.each do |song|
+    data = JSON.parse(redis.get(song))
+    data['artist'] = data['artist'].gsub(':', ' ')
+    data['song'] = data['song'].gsub(':', ' ')
+    begin
     tracks = RSpotify::Track.search(data['song'] + " " + data['artist'], limit: 1)
     if (tracks.total == 0)
       puts data
@@ -60,10 +58,11 @@ list.each do |song|
     else
       playlist_tracks.push(tracks.first)
     end
-  rescue
+    rescue
     puts data
+    end
+
   end
 
+  playlist.replace_tracks!(playlist_tracks)
 end
-
-playlist.replace_tracks!(playlist_tracks)
